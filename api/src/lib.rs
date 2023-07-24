084d53c236a623fe3a4d7c399e0d49a9618f0ad6
N <mail@nuhhtyy.xyz>
2023-07-24 11:52:08 -0400

    typo

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,7 +1,7 @@
 /// the [Client] trait, an [async_trait::async_trait] for an abstract client over some type of endpoint
 pub mod client;
 
-/// a default implemantion of [Client<Order>] around the original uniswapX api
+/// a default implementation of [Client<Order>] around the original uniswapX api
 pub mod uniswap;
 
 /// implementaion of [OrderSubscriber] which wraps a [Client<Order>]
